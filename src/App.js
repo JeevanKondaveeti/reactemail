e17a8ff6d98fb3659a034197e978209a6d585c59
Jeevan Kondaveeti <kondaveetijeevan@gmail.com>
2024-07-03 06:43:29 +0530

    ui modification in form used material UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
+import Button from '@mui/material/Button';
+import { TextField } from '@mui/material';
 
 function App(){
   const form = useRef();
@@ -13,7 +15,8 @@ function App(){
       })
       .then(
         () => {
-          console.log('SUCCESS!');
+          console.log(e);
+          alert('SUCCESS!');
         },
         (error) => {
           console.log('FAILED...', error.text);
@@ -24,16 +27,13 @@ function App(){
   return (
     <center>
     <form ref={form} onSubmit={sendEmail} style={{display:"flex" , width:"50%" ,
-      flexDirection:"column" , margin:"5%" , border:"2px solid blue" , padding:"6%"}}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
+      flexDirection:"column" , margin:"5%" , border:"1px solid grey" , padding:"6%"}}>
+      <TextField id="" label="Name" variant="outlined" name="user_name" style={{padding:"1%"}}/>
       <br />
-      <label>Email</label>
-      <input type="email" name="user_email" />
+      <TextField id="outlined-basic" label="Email" variant="outlined" name="user_email" style={{padding:"1%"}}/>
       <br />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
+      <TextField id="outlined-basic" label="Message" variant="outlined" name="message" multiline maxRows={4} style={{padding:"1%"}}/>
+      <Button variant="contained" type="submit" value="Send">Submit</Button>
       <br />
     </form>
     </center>
